c64f3283685262d084ae8d3c24e2cb4b6f2f1d5a
YangzJ <809318068@qq.com>
2018-07-21 00:35:58 +0800

    init

---
+++ src/main/java/com/seawaterbt/ssm/SsmApplication.java
@@ -0,0 +1,12 @@
+package com.seawaterbt.ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmApplication.class, args);
+    }
+}
